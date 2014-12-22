74b47e83a9ec030be1743c9d564ed44a246da99a
Mehrdad Tahernia <mehrdad.tahernia@gmail.com>
2014-12-22 11:24:29 +0800

    Seperated GFq class into seperate files

---
+++ GFq.h
@@ -0,0 +1,216 @@
+/*
+ * GFq.h
+ *
+ *  Created on: 22 Dec, 2014
+ *      Author: Mehrdad Tahernia
+ *		  User: mehrdad
+ */
+
+#ifndef GFQ_H_
+#define GFQ_H_
+
+#include "Functions.h"
+
+/************************************************************************
+ *
+ * GF(q)
+ *
+ ************************************************************************/
+class GFq {
+public:
+	static int q;					// General field definition parameter
+	static int log_2_q; 		    // If q is not prime
+	static int mask;		        // if q is not prime - 1's at first positions
+	static GFq alpha[MAX_Q];		// alpha[i] is the ith element of field
+	static int reverse_alpha[MAX_Q];// multiplicative inverse of alphas
+	static GFq inverse[MAX_Q];		// inverse[i] multiplicative inverse of i
+	static BOOLEAN IsPrimeQ;		// is our field prime, extension or invalid
+	static BOOLEAN IsModuloOperations; // when we have a prime field, operations become modular
+
+	// Value of field element
+	int val;
+
+public:
+	// GFq constructor with value g
+	GFq(GFq &g) : val(g.val) {	}
+
+	BOOLEAN IsZero() {
+		return val == 0;
+	}
+
+	BYTE GetVal() { // Unsigned Char
+		return val;
+	}
+
+	static void Initialize(int p_q);
+
+	static void GenerateAlphas(int m);
+
+	static GFq &One() {
+		if (IsModuloOperations) {
+			static GFq ConstOne(1);
+			return ConstOne;
+		} else
+			return alpha[0];
+	}
+
+	BOOLEAN operator==(GFq g) {
+		return val == g.val;
+	}
+
+	GFq(int i) :
+			val((BYTE) i) {
+	}
+	GFq(BYTE b = 0) :
+			val(b) {
+	}
+
+	GFq &operator+=(GFq g) {
+		if (IsModuloOperations)
+			val = (val + g.val) % q;
+		else
+			val ^= g.val;
+
+		return *this;
+	}
+
+	GFq &operator=(GFq &g) {
+		val = g.val;
+		return *this;
+	}
+
+	GFq &operator=(BYTE b) {
+		val = b;
+		return *this;
+	}
+
+	GFq &operator=(int i) {
+		val = (BYTE) i;
+		return *this;
+	}
+
+	GFq &operator-=(GFq g) {
+		if (IsModuloOperations)
+			val = (q + val - g.val) % q;
+		else
+			val ^= g.val;
+
+		return *this;
+	}
+
+	GFq &operator*=(GFq g) {
+		if ((val == 0) || (g.val == 0))
+			val = 0;
+		else {
+			if (IsModuloOperations) {
+				if (!IsPrimeQ) {
+					cout << "GFq::operator*=: Invalid multiplication (q is not prime)\n";
+					exit(1);
+				}
+
+				val = (val * g.val) % q;
+			} else
+				val = alpha[(reverse_alpha[this->val] + reverse_alpha[g.val])
+						% (q - 1)].val;
+		}
+
+		return *this;
+	}
+
+	GFq &Inverse() {
+		return inverse[val];
+	}
+
+	GFq &Minus() {
+		static GFq g;
+
+		if (IsModuloOperations)
+			g.val = (q - val) % q;
+		else
+			// GF(2^m)
+			g.val = val;
+
+		return g;
+	}
+
+	void RandomSelect()      // Random select among nonzero elements
+	{
+		val = uniform_random(q - 1) + 1;
+	}
+
+	GFq &operator/=(GFq g) {
+		if (val == 0)
+			val = 0;
+		else if (g.val == 0) {
+			cout << "GFq::operator /= division by zero\n";
+			exit(1);
+		} else {
+			if (IsModuloOperations) {
+				if (!IsPrimeQ) {
+					cout
+							<< "GFq::operator*=: Invalid multiplication (q is not prime)\n";
+					exit(1);
+				}
+
+				*this *= inverse[g.val];
+			} else
+				val = alpha[(q - 1
+						+ (reverse_alpha[this->val] - reverse_alpha[g.val]))
+						% (q - 1)].val;
+		}
+
+		return *this;
+	}
+
+	GFq &operator*(GFq g) {
+		static GFq result;
+
+		result = *this;
+		result *= g;
+
+		return result;
+	}
+
+	GFq &operator/(GFq g) {
+		static GFq result;
+
+		result = *this;
+		result /= g;
+
+		return result;
+	}
+
+	GFq &operator+(GFq g) {
+		static GFq result;
+
+		result = *this;
+		result += g;
+
+		return result;
+	}
+
+	GFq &operator-(GFq g) {
+		static GFq result;
+
+		result = *this;
+		result -= g;
+
+		return result;
+	}
+};
+
+inline std::ostream &operator<<(std::ostream &s, GFq g) {
+	s << (int) g.val;
+
+	return s;
+}
+
+inline std::istream &operator>>(std::istream &s, GFq &g) {
+	int i;
+	s >> i;
+
+	g.val = i;
+
+	return s;
+}
+#endif /* GFQ_H_ */
--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 LIBS = -lm -lfftw3 -static -static-libgcc -static-libstdc++
 CCC = g++ -O3 -Wall -Wno-deprecated -DLINUX_C
-OBJ_FILES = Report.o Utils_2.o LDPC_1.o LDPC_2.o Encoding.o Main.o
-H_FILES = Functions.h Utils_2.h LDPC_1.h LDPC_2.h Encoding.h Portability.h Channel.h Report.h Definitions.h
+OBJ_FILES = GFq.o Report.o Utils_2.o LDPC_1.o LDPC_2.o Encoding.o Main.o
+H_FILES = Functions.h Utils_2.h LDPC_1.h LDPC_2.h Encoding.h Portability.h Channel.h Report.h Definitions.h GFq.h
 
 all: q_LDPC
 
 Report.o: Report.cpp ${H_FILES}
 	${CCC} -c Report.cpp -o Report.o
+GFq.o: GFq.cpp ${H_FILES}
+	${CCC} -c GFq.cpp -o GFq.o
 Utils_2.o: Utils_2.cpp ${H_FILES}
 	${CCC} -c Utils_2.cpp -o Utils_2.o
 LDPC_1.o: LDPC_1.cpp ${H_FILES}

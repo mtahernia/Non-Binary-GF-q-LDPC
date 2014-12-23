81e6355b0e090c256bc4a503a5ab0456a28723aa
Mehrdad Tahernia <mehrdad.tahernia@gmail.com>
2014-12-23 17:15:37 +0800

    Seperated TopList Class, It does not have an implementation file, and is not used anywhere

--- LDPC_2.h
+++ LDPC_2.h
@@ -15,7 +15,7 @@
 #include "Variable_Node.h"
 #include "Check_Node.h"
 #include "Edge.h"
-
+//#include "TopList.h" // Not Used
 
 
 /*****************************************************************************
@@ -63,9 +63,7 @@ public:
 					NULL), EdgeStack(NULL) {
 	}
 
-	~bipartite_graph() {
-		Clear();
-	}
+	~bipartite_graph() { Clear();}
 
 	void Clear() {
 		if (variable_nodes != NULL)
@@ -107,75 +105,6 @@ public:
 	}
 };
 
-/************************************************************************
- *
- * TopList - for use in greedy
- *
- ************************************************************************/
-// TODO: I don't Understand
-class TopList {
-public:
-	GFq *BestChange;
-	int *VariableIndex;
-	double *DistortionImprovement;
 
-	int MaxSize, CurrentSize;
-public:
-	TopList() :
-			MaxSize(-1), CurrentSize(-1) {
-	}
-
-	~TopList() {
-		DeAllocate();
-	}
-
-	void DeAllocate() {
-		if (MaxSize > -1) {
-			delete BestChange;
-			delete VariableIndex;
-			delete DistortionImprovement;
-		}
-		MaxSize = -1;
-	}
-
-	void Init(int p_MaxSize) {
-		if (MaxSize != p_MaxSize) {
-			DeAllocate();
-
-			BestChange = new GFq[p_MaxSize];
-			VariableIndex = new int[p_MaxSize];
-			DistortionImprovement = new double[p_MaxSize];
-		}
-
-		MaxSize = p_MaxSize;
-		CurrentSize = 0;
-	}
-
-	void Add(GFq p_BestChange, int p_VariableIndex,
-			double p_DistortionImprovement) {
-		// Find place for new distortion
-		int Place;
-		for (Place = 0; Place < CurrentSize; Place++)
-			if (p_DistortionImprovement > DistortionImprovement[Place])
-				break;
-
-		// Shift all the rest one place up
-		if (CurrentSize < MaxSize)
-			CurrentSize++;
-		for (int i = CurrentSize - 1; i > Place; i--) {
-			BestChange[i] = BestChange[i - 1];
-			VariableIndex[i] = VariableIndex[i - 1];
-			DistortionImprovement[i] = DistortionImprovement[i - 1];
-		}
-
-		// Place new entry
-		if (Place < CurrentSize)      // If not passed last place in TopList
-				{
-			BestChange[Place] = p_BestChange;
-			VariableIndex[Place] = p_VariableIndex;
-			DistortionImprovement[Place] = p_DistortionImprovement;
-		}
-	}
-};
 
 #endif
